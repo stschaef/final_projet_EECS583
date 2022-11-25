aafc5a5e06fde5cca03df6308a674e536d434907
Steven Schaefer <stschaef@eecs583a.eecs.umich.edu>
2022-11-24 20:35:39 -0500

    can print simple dependence info

--- polly_tests/run_polly.sh
+++ polly_tests/run_polly.sh
@@ -1,9 +1,7 @@
 #!/bin/bash
 
 /n/eecs583b/home/stschaef/llvm_polly/bin/opt \
- -basic-aa \
- -polly-ast \
+ -polly-process-unprofitable \
+ -polly-dependences \
  -analyze \
  example.preopt.ll \
- -polly-process-unprofitable \
- -polly-use-llvm-names \
